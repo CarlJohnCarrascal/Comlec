9f340088282127090b77cd665170a34c9f06d9c1
cajsen <carrascalcarljohn1@gmail.com>
2023-12-02 11:06:58 +0800

    sdsd

---
+++ comlec_front/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <a v-on:click="$router.push('/home')" class="btn">Home</a>
+    <a v-on:click="$router.push('/list')" class="btn">List</a>
+  </header>
+
+  <main>
+    <RouterView/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ comlec_front/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import List from '../views/List.vue'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { 
+            path: '/',
+            alias: '/home' ,
+            component: Home,
+            meta: {
+                title: "Home",
+                button: "btn-home",
+            }
+        },
+        { 
+            path: '/list', 
+            component: List,
+            meta: {
+                title: 'List',
+                button: "btn-dashboard",
+            }
+        }
+      ]
+  })
+
+  export default router
